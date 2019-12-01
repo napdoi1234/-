4d2923db1697b100650f334959a7a42582e972c2
napdoi1234 <53718542+napdoi1234@users.noreply.github.com>
2019-12-01 20:56:42 +0700

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,165 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt,{
+        useminPrepare:'grunt-usemin'
+    });
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        },
+        copy:{
+            html:{
+                files:[
+                    {
+                        expand:true,
+                        dot:true,
+                        cwd:'./',
+                        src:['*.html'],
+                        dest:'dist'
+                    }
+                ]
+            },
+            fonts:{
+                files:[{
+                    expand:true,
+                    dot:true,
+                    cwd:'node_mdules/font-awesome',
+                    src:['fonts/*.*'],
+                    dest:'dist'
+                }]
+            }
+        },
+        clean:{
+            build:{
+                src:['dist/']
+            }
+        },
+        imagemin:{
+            dynamic:{
+                files:[{
+                    expand:true,
+                    cwd:'./',
+                    src:['img/*.{png,jpg,gif}'],
+                    dest:'dist'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js:['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                            var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }       
+                        }]
+                    }
+                }
+            }
+        },
+        concat:{
+            options:{
+                separator:';'
+            },
+            dist:{}
+        },
+        uglify:{
+            dist:{}
+        },
+        cssmin:
+  { dist: { options: { report: 'gzip' } },
+  'dist/css/main.css': 'dist/css/main.css' },
+        filerev:{
+            options:{
+                encoding:'utf8',
+                algorithm:'md5',
+                length:20
+            },
+            release:{
+                files:[{
+                    src:[
+                        'dist/js/*.js',
+                        'dist/css/*.css',
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css','dist/js']
+            }
+        },
+        htmlmin:{
+            dist:{
+                options:{
+                    collapseWhitespace:true
+                },
+                files:{
+                    'dist/index.html':'dist/index.html',
+                    'dist/contactus.html':'dist/contactus.html',
+                    'dist/aboutus.html':'dist/aboutus.html',
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
+};
